60d169a6f687bca89d7027d396a341210c72be65
Mariya Miteva <mimi_9204@abv.bg>
2013-06-15 22:36:34 +0300

    initial commit
    lein new quarto app

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to quarto
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/quarto/core_test.clj
@@ -0,0 +1,7 @@
+(ns quarto.core-test
+  (:use clojure.test
+        quarto.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
