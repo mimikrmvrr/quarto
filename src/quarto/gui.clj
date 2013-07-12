d04386d7a2f893d837114463251463e7cf608a70
Mariya Miteva <mimi_9204@abv.bg>
2013-07-12 09:31:59 +0300

    add background to piece-button

--- src/quarto/gui.clj
+++ src/quarto/gui.clj
@@ -50,7 +50,8 @@
   (let [panel (JPanel.)
         button (doto (JButton. (ImageIcon. (io/resource "p0e.png")))
                ; (.setSize (Dimension. 50 75)))]
-                (.setBounds 520 135 50 75))]
+                (.setBounds 520 135 50 75)
+                (.setBackground (Color. 241 221 196 255)))]
         ; name-field1 (JTextField. "Player1" 20)
         ; name-field2 (JTextField. "Player2" 20)
         ; submit-button (JButton. "OK")
